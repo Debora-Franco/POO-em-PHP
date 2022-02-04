d5b2dcad65ea001f9abf78243c392d077873354d
Débora Souza <debora-souza14@hotmail.com>
2022-02-04 16:59:00 +0000

    Add files via upload

---
+++ Aula06/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1> Projeto Controle Remoto </h1>
+         <pre>
+            <?php
+       
+        require_once 'ControleRemoto.php';
+        $c=new ControleRemoto();
+        $c->ligar();
+        $c->maisVolume();
+        $c->abrirMenu();
+       
+        ?>
+ </pre>
+    </body>
+</html>
